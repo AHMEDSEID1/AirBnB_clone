5ba9ed729655e3103f1fb29254efdf9de6faeccd
amigaye <mulerami9@gmail.com>
2024-01-15 00:57:49 -0800

    AirBnB clone

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -15,6 +16,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
         else:
             f = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
@@ -27,6 +29,7 @@ class BaseModel:
         """save def
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ to_dict def
