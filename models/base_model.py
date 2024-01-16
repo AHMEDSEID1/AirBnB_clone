c89ad27555caff91f2491ecc8051226358a5a0cc
amigaye <mulerami9@gmail.com>
2024-01-16 00:38:05 -0800

    AirBnB clone

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 """
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 
 class BaseModel:
@@ -16,7 +16,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
+            storage.new(self)
         else:
             f = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
@@ -29,7 +29,7 @@ class BaseModel:
         """save def
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """ to_dict def
--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import models
+from models import storage
 from models.base_model import BaseModel
 
-all_objs = models.storage.all()
+all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
